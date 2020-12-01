7ad6c40f47cec8298aaed0183bfff4b4463c824c
Shahid Saddique <shahidali6@gmail.com>
2020-12-02 01:52:46 +0500

    Add function of ScrollToElement

--- CommonClasses/Selenium.cs
+++ CommonClasses/Selenium.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Threading;
 
 namespace CommonClasses
 {
-    class Selenium
+    public class Selenium
     {
+        public static void ScrollToElement(ChromeDriver driver, string id, int scrollTimes)
+        {
+            Random randomValue = new Random();
+            int randomNumber = randomValue.Next(3000, 5000);  // creates a number between 3 and 6 Seconds
+            //var elementScroll = driver.FindElement(By.Id("menu-footer-menu"));
+            var elementScroll = driver.FindElement(By.Id(id));
+            Actions actions = new Actions(driver);
+            for (int i = 0; i < scrollTimes; i++)
+            {
+                actions.MoveToElement(elementScroll);
+                actions.Perform();
+                Thread.Sleep(randomNumber);
+                int loopCounter = i + 1;
+                Console.WriteLine("Loop Index: [" + loopCounter+ " of " + scrollTimes + "]");
+            }
+        }
     }
 }
