62960a4be45a92619dc51749482ebf1bcd7d77a0
Shahid Saddique <shahidali6@gmail.com>
2020-12-02 09:34:42 +0500

    3 basic functions added.
    LoadPagefromURL
    LoadPagefromBrower
    LoadPagefromHTMLFile

--- CommonClasses/HTMLAgilityPack.cs
+++ CommonClasses/HTMLAgilityPack.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,23 @@ namespace CommonClasses
 {
     class HTMLAgilityPack
     {
+        public HtmlDocument LoadPagefromURL(string url)
+        {
+            HtmlWeb webAgent = new HtmlWeb();
+            HtmlDocument htmlDocument = webAgent.Load(url);
+            return htmlDocument;
+        }
+        public HtmlDocument LoadPagefromBrower(string url)
+        {
+            HtmlWeb webAgent = new HtmlWeb();
+            HtmlDocument htmlDocument = webAgent.LoadFromBrowser(url);
+            return htmlDocument;
+        }
+        public HtmlDocument LoadPagefromHTMLFile(string path)
+        {
+            HtmlDocument htmlDocument = new HtmlDocument();
+            htmlDocument.Load(path);
+            return htmlDocument;
+        }
     }
 }
