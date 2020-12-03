9d3517aeeb76004f9b536acf49b53baf1dbc4bb0
Shahid Saddique <shahidali6@gmail.com>
2020-12-03 12:54:41 +0500

    1. remove unused usings 2. private to public class HTMLAgilityPack 3. create public list property public List<string> FirstName 4. Function change to public and static HtmlDocument LoadPagefromURL(string url) 5. Function change to static HtmlDocument LoadPagefromBrower(string url), HtmlDocument LoadPagefromHTMLFile(string path) 6. New function added.  a. public static HtmlNodeCollection FilterHTMLTagsFromSource(HtmlDocument htmlSource, string xPath)  b. public static List<string> GetAllCourseURLstoList(HtmlNodeCollection allNodes, bool displayConsoleData)  c. public static List<string> GetAllCourseNamestoList(HtmlNodeCollection allNodes, bool displayConsoleData)  d. public static List<CourseNameAndURL> GetAllCourseNamesAndURLstoList(HtmlNodeCollection allNodes, List<string> listOfCourseNames, List<string> listOfCourseURLs) 7. Structure added.  a. public struct CourseNameAndURL

--- CommonClasses/HTMLAgilityPack.cs
+++ CommonClasses/HTMLAgilityPack.cs
@@ -1,31 +1,86 @@
 ﻿using HtmlAgilityPack;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CommonClasses
 {
-    class HTMLAgilityPack
+    public class HTMLAgilityPack
     {
-        public HtmlDocument LoadPagefromURL(string url)
+        public List<string> FirstName { get; set; }
+        public static HtmlDocument LoadPagefromURL(string url)
         {
             HtmlWeb webAgent = new HtmlWeb();
             HtmlDocument htmlDocument = webAgent.Load(url);
             return htmlDocument;
         }
-        public HtmlDocument LoadPagefromBrower(string url)
+        public static HtmlDocument LoadPagefromBrower(string url)
         {
             HtmlWeb webAgent = new HtmlWeb();
             HtmlDocument htmlDocument = webAgent.LoadFromBrowser(url);
             return htmlDocument;
         }
-        public HtmlDocument LoadPagefromHTMLFile(string path)
+        public static HtmlDocument LoadPagefromHTMLFile(string path)
         {
             HtmlDocument htmlDocument = new HtmlDocument();
             htmlDocument.Load(path);
             return htmlDocument;
+        }
+        public static HtmlNodeCollection FilterHTMLTagsFromSource(HtmlDocument htmlSource, string xPath)
+        {
+            HtmlNodeCollection htmlNodes = htmlSource.DocumentNode.SelectNodes(xPath);
+            return htmlNodes;
+        }
+        public static List<string> GetAllCourseURLstoList(HtmlNodeCollection allNodes, bool displayConsoleData)
+        {
+            List<string> listOfCourseURLs = new List<string>();
+            foreach (var node in allNodes)
+            {
+                listOfCourseURLs.Add(node.GetAttributeValue("href", String.Empty));
+                //Console.WriteLine(node.Attributes["href"].Value);
+                if (displayConsoleData)
+                {
+                    Console.WriteLine(node.GetAttributeValue("href", String.Empty));
+                }
+            }
+            return listOfCourseURLs;
+        }
+        public static List<string> GetAllCourseNamestoList(HtmlNodeCollection allNodes, bool displayConsoleData)
+        {
+            List<string> listOfCourseNames = new List<string>();
+            foreach (var node in allNodes)
+            {
+                string refineHtml = node.InnerText;
+                refineHtml = HTMLCodeSnippet.ReplaceSpecialCharacters(refineHtml);
+                listOfCourseNames.Add(refineHtml);
+                if (displayConsoleData)
+                {
+                    Console.WriteLine(refineHtml);
+                }
+            }
+            return listOfCourseNames;
+        }
+        public static List<CourseNameAndURL> GetAllCourseNamesAndURLstoList(HtmlNodeCollection allNodes, List<string> listOfCourseNames, List<string> listOfCourseURLs)
+        {
+            List<CourseNameAndURL> CousreNameURLList = new List<CourseNameAndURL>();
+            CourseNameAndURL CourseNameURL = new CourseNameAndURL();
+            foreach (var node in allNodes)
+            {
+                string refineHtml = node.InnerText;
+                refineHtml = HTMLCodeSnippet.ReplaceSpecialCharacters(refineHtml);
+                CourseNameURL.courseName = refineHtml;
+                Console.WriteLine(refineHtml);
+                CourseNameURL.cousreURL = node.GetAttributeValue("href", String.Empty);
+                //listOfCourseURLs.Add(node.GetAttributeValue("href", String.Empty));
+                //Console.WriteLine(node.Attributes["href"].Value);
+                Console.WriteLine(node.GetAttributeValue("href", String.Empty));
+                CousreNameURLList.Add(CourseNameURL);
+            }
+            return CousreNameURLList;
+        }
+        public struct CourseNameAndURL
+        {
+            public string courseName;
+            public string cousreURL;
         }
     }
 }
